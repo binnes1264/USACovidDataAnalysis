e48c9af73643fa4cde2f6716a99bb9a39f7d2c67
binnes1264 <101842768+binnes1264@users.noreply.github.com>
2023-09-07 12:16:41 +0200

    Update datacharts2022.py

--- datacharts2022.py
+++ datacharts2022.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#create graph instance with characteristics
 def createVisual(x_Cord, y_Cord, x_Label, y_Label, title):
     plt.bar(x_Cord, y_Cord)
     plt.gcf().set_size_inches(8, 4)
@@ -9,19 +10,24 @@ def createVisual(x_Cord, y_Cord, x_Label, y_Label, title):
     plt.title(title)
     plt.show()
   
-
+#open cleaned file
 df_old = pd.read_csv('New_United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv')
 
+#create file for final data
 df_new = pd.DataFrame()
 
+#create new columns with the max confirmed cases and deaths by state
 df_new['maxCase'] = df_old.groupby(['state'])['conf_cases'].max()
 df_new['maxDeath'] = df_old.groupby(['state'])['conf_death'].max()
 
+#add calulated max values to final file
 df_new.to_csv('Final_United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv')
 
+#add state names to final file
 states = []
 states = df_new.index
 
+#create graphs
 createVisual(states, df_new['maxCase'], 'States', 'Confirmed Reported Cases(millions)', 
              'COVID-19 Cases per State (that chose to report)')
 createVisual(states, df_new['maxDeath'], 'States', 'Confirmed Reported Deaths', 
